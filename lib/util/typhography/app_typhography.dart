5ac476e6cd0d6e547b7532443c60fe18d2ed2134
RulH19 <nurulh298hd@gmail.com>
2025-05-21 14:36:01 +0700

    prepare project

---
+++ lib/util/typhography/app_typhography.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTyphography {
+  static final _commonPoppinsStyle = GoogleFonts.poppins();
+  static final _commonHinduGunturStyle = GoogleFonts.hindGuntur();
+
+  static TextStyle headlineHigh = _commonPoppinsStyle.copyWith(
+    color: Colors.white,
+    fontSize: 30,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0,
+    height: 1.0,
+  );
+  static TextStyle headlineSmall = _commonPoppinsStyle.copyWith(
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0,
+    height: 1.0,
+  );
+  static TextStyle kontenHigh = _commonHinduGunturStyle.copyWith(
+    color: Colors.white,
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.01,
+    height: 1.5,
+  );
+  static TextStyle kontenSmall = _commonHinduGunturStyle.copyWith(
+    color: Colors.white,
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0,
+    height: 1.5,
+  );
+}
